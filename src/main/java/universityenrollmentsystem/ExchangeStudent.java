5f5e20327b5b15be9c42c7609a4929fdad42b77f
Delaksan-Sritharan <delaksan@gmail.com>
2025-12-04 12:15:30 +0530

    Feat: Implement duration validation, long-term status check, and enhanced output
    Added getters/setters for new attributes, including validation for exchangeDurationMonths (minimum 1 month).
    
    Implemented isLongTermExchange() method to check for durations of 6+ months.
    
    Overrode toString() to display key details (university, country, duration) and long/short-term status.

--- src/main/java/universityenrollmentsystem/ExchangeStudent.java
+++ src/main/java/universityenrollmentsystem/ExchangeStudent.java
@@ -6,10 +6,12 @@ public class ExchangeStudent extends Student{
     private int exchangeDurationMonths;
     private boolean hasScholarship;
 
-    public ExchangeStudent(String name, String surname,String homeUniversity) {
+    public ExchangeStudent(String name, String surname,String homeUniversity,String homeCountry) {
         super(name, surname);
         this.homeUniversity = homeUniversity;
         this.homeCountry = homeCountry;
+        this.exchangeDurationMonths = 1;
+        this.hasScholarship = false;
     }
 
     public String getHomeUniversity() {
@@ -33,7 +35,12 @@ public class ExchangeStudent extends Student{
     }
 
     public void setExchangeDurationMonths(int exchangeDurationMonths) {
-        this.exchangeDurationMonths = exchangeDurationMonths;
+        if (exchangeDurationMonths < 1){
+            System.out.println("Exchange duration must be at least 1 month. Setting to 1.");
+            this.exchangeDurationMonths = 1;
+        }else {
+            this.exchangeDurationMonths = exchangeDurationMonths;
+        }
     }
 
     public boolean isHasScholarship() {
@@ -44,9 +51,20 @@ public class ExchangeStudent extends Student{
         this.hasScholarship = hasScholarship;
     }
 
+    public boolean isLongTermExchange(){
+        return exchangeDurationMonths >=6;
+    }
+
     @Override
     public String toString(){
-        return super.toString();
+        String durationType = isLongTermExchange()? "Long-term": "Short-term";
+        return super.toString()+
+                "\nHome University: " + homeUniversity +
+                "\nHome Country: " + homeCountry +
+                "\nExchange Duration: " + exchangeDurationMonths + " months" +
+                "\nExchange Type: " + durationType +
+                "\nHas Scholarship: " + (hasScholarship ? "Yes" : "No");
+
     }
 
 }
